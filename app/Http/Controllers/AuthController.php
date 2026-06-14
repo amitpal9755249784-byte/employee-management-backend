a4b35a76f8b39052a1e918cf354cc66cde99d8aa
Amit Baghel <amitpal9755249784@gmail.com>
2026-06-14 22:02:04 +0530

    Debug login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,24 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'message' => 'Invalid Credentials'
-            ], 401);
-        }
-
         return response()->json([
-            'message' => 'Login Successful',
-            'user' => $user
+            'email' => $request->email,
+            'password' => $request->password
         ]);
     }
 }
