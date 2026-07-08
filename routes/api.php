d74aea765419a597b3e7a0d52010504c1b8b1e2d
Amit Baghel <amitpal9755249784@gmail.com>
2026-07-08 16:53:05 +0530

    Add change password route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\EmployeeController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::put('/profile/{id}', [AuthController::class, 'updateProfile']);
+Route::put('/change-password/{id}', [AuthController::class, 'changePassword']);
 
 Route::get('/employees', [EmployeeController::class, 'index']);
 Route::post('/employees', [EmployeeController::class, 'store']);
